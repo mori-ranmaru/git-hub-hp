78a3fed84dae0f79123aa423356dd106129e95c5
mori-ranmaru <harukisoccer0414@icloud.com>
2024-07-23 00:49:27 +0900

    メニューリンク

---
+++ php/task1-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php練習</title>
+</head>
+<body>
+  <?php
+  echo "暑中お見舞い申し上げます。<br>
+  <br>暑中お見舞い
+  <br>申し上げます。";
+  ?>
+ 
+</body>
+</html>
---
+++ php/task1-2-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    define("GREETING", "Hello, World!");
+    ?>
+
+
+現在、消費税は10％です。
+鉛筆が100円で税込110円です。
+消しゴムが200円で税込220円です。
+</body>
+</html>
